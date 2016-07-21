28b385b2619c121ca0ac9f98f39fbb2ee37e3a6c
Brandon Clapp <clapp09@gmail.com>
2016-07-21 11:18:00 -0500

    formatting

--- http-csrf.js
+++ http-csrf.js
@@ -1,35 +1,35 @@
-(function (angular) {
-    'use strict'
-    angular.module('CSRFToken', [])
-		.provider('CSRFToken', ['$httpProvider', function ($httpProvider) {
+(function(angular) {
+  'use strict'
+  angular.module('CSRFToken', [])
+    .provider('CSRFToken', ['$httpProvider', function($httpProvider) {
 
-		    var csrfToken;
-		    var header = 'X-XSRF-Token';
+      var csrfToken;
+      var header = 'X-XSRF-Token';
 
-		    this.useToken = function (token, customHeaderName) {
+      this.useToken = function(token, customHeaderName) {
 
-		        if (customHeaderName) {
-		            header = customHeaderName;
-		        }
+        if (customHeaderName) {
+          header = customHeaderName;
+        }
 
-		        if (!token) {
-		            console.warn('No CSRF token was provided.');
-		        }
+        if (!token) {
+          console.warn('No CSRF token was provided.');
+        }
 
-		        $httpProvider.defaults.headers.common[header] = token;
-		    }
+        $httpProvider.defaults.headers.common[header] = token;
+      }
 
-		    this.$get = function () {
-		        return {
-		            getToken: function () {
-		                return csrfToken;
-		            },
-		            getHeader: function () {
-		                return header;
-		            }
-		        };
-		    }
+      this.$get = function() {
+        return {
+          getToken: function() {
+            return csrfToken;
+          },
+          getHeader: function() {
+            return header;
+          }
+        };
+      }
 
-		    return this;
-		}]);
+      return this;
+    }]);
 })(angular);
